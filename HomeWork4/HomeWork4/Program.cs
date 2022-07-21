b1bbdc0a7563ac50295f44325b586177e5498cab
unknown <danenkov.dmitriy@list.ru>
2022-07-21 22:01:38 +0300

    loading 999999

---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press the length of Array");
+            int lengthOfArray = int.Parse(Console.ReadLine());
+            int[] ints1 = new int[lengthOfArray];
+            int summ = 0;
+            int Chet = 0;
+            
+            for (int i = 0; i < ints1.Length; i++)
+            {
+                Console.WriteLine($"Press {i} element of arrray:");
+                ints1[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Your array is: ");
+            foreach (int timelapse in ints1)
+            {
+                Console.Write($"{timelapse}   ");
+            }
+            Console.WriteLine();
+            for (int z = 0; z <= ints1.Length - 1; z = z + 2)
+            {
+                summ += ints1[z];
+            }
+            Console.WriteLine($"Summ of odd elements of Array : {summ}");
+            for (int i = 0; i <ints1.Length; i++)
+            {
+                if (ints1[i] % 2 == 0)
+                {
+                    Chet += ints1[i];
+                }
+            }
+            Console.WriteLine($"Summ of even numbers in Array (which you entered): {Chet}");
+            int minimus = ints1[0];
+            for (int i = 0; i < ints1.Length; i++)
+            {
+                if (ints1[i] < minimus)
+                {
+                   minimus = ints1[i];
+                }
+                
+            }
+            Console.WriteLine($"minimal number of Array is {minimus}");
+        }
+    }
+}
---
+++ HomeWork4_1/HomeWork4_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite razmernots' massiva");
+            int N = int.Parse(Console.ReadLine());
+            int[] massiv = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"Press {i} element of Array");
+                massiv[i] = int.Parse(Console.ReadLine()); 
+            }
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < N; i++)
+            {
+                stack.Push(massiv[i]);          
+            }
+            Console.WriteLine("Вывод стака");
+            foreach (int number in stack)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine($"stack consist of {stack.Count} elements");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine($" Posle POP stack consist of {stack.Count} elements");
+        }
+    }
+}
